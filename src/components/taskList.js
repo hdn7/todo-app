6fcd45f02d35337a8aacfeeb28414592d05e3401
Hieu Do Ngoc <hieudn.uh@gmail.com>
2020-06-16 12:51:03 -0400

    feat: add empty task list screen

---
+++ src/components/taskList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ClipboardGray from '../assets/images/clipboard-gray.png';
+
+const TaskList = () => {
+  return (
+    <div className="flex flex-col justify-center items-center fixed w-full h-screen">
+      <img src={ClipboardGray} alt="" className="w-30 mt-10 pointer-events-none" />
+      <div className="block mt-12 text-center">
+        <div className="text-xl tracking-tight text-blue-900 font-medium">No tasks</div>
+        <div className="text-base text-gray-500 opacity-50 font-normal">
+          You have no tasks to do.
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TaskList;
